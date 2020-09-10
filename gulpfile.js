ebe9146ab227868a732c81e48314d4340784978e
bearbearbear <ghlwkdthfl@gmail.com>
2020-09-11 02:13:48 +0900

    feat: eslint 추가, 오류 픽스 미완료

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 const gulp = require("gulp");
 const babel = require("gulp-babel");
+const eslint = require("gulp-eslint");
 
 // 걸프 의존성 쓰는 곳
 
 gulp.task("default", (done) => {
+  // Eslint 실행
+  gulp
+    .src(["es6/**/*.js", "public/es6/**/*.js"])
+    .pipe(eslint())
+    .pipe(eslint.format());
+
   // 노드 소스
   gulp.src("es6/**/*.js").pipe(babel()).pipe(gulp.dest("dist"));
 
