0fdf5f108a7dcd56311d48becb3cf82291912108
bearbearbear <ghlwkdthfl@gmail.com>
2020-09-10 23:08:22 +0900

    feat: gulp-babel 추가

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 const gulp = require("gulp");
+const babel = require("gulp-babel");
 
 // 걸프 의존성 쓰는 곳
 
 gulp.task("default", (done) => {
-  // 걸프 작업 쓰는 곳
+  // 노드 소스
+  gulp.src("es6/**/*.js").pipe(babel()).pipe(gulp.dest("dist"));
+
+  // 브라우저 소스
+  gulp.src("public/es6/**/*.js").pipe(babel()).pipe(hulp.dest("public/dist"));
+
   done();
 });
