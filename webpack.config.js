0f822a4f590b7291150a11e38f3d9d023462e2e5
bearbearbear <ghlwkdthfl@gmail.com>
2020-09-11 22:50:59 +0900

    feat: webpack 기본 구조 구현

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,11 @@
 const path = require("path");
 
 module.exports = {
-  // enntry file
-  entry: "./src/js/main.js",
-  // 컴파일 + 번들링된 js 파일이 저장될 경로와 이름 지정
+  entry: {
+    main: "./src/es6/main.js",
+  },
   output: {
-    path: path.resolve(__dirname, "dist/js"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, "./public/src/es5"),
+    filename: "main.js",
   },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: [path.resolve(__dirname, "src/js")],
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-            plugins: ["@babel/plugin-proposal-class-properties"],
-          },
-        },
-      },
-    ],
-  },
-  devtool: "source-map",
-  // https://webpack.js.org/concepts/mode/#mode-development
-  mode: "development",
 };
