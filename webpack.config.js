be2c5da24513b062bd963fc9109e188dda40677b
bearbearbear <ghlwkdthfl@gmail.com>
2020-09-12 00:20:47 +0900

    feat: 웹팩 js파일 엔트리 파일과 아웃풋 경로 설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,48 @@
 const path = require("path");
+const ENTRY_FILE = path.resolve(__dirname, "src", "es6", "main.js");
+const OUTPUT_DIR = path.resolve(__dirname, "public", "src", "es5");
+const MODE = "development";
+
+// const exclude = {
+//   exportList: function (ext) {
+//     const path = require("path");
+
+//     for (let [name, list] of Object.entries(this)) {
+//       if (name === ext) {
+//         let absolutePath = path.resolve(__dirname, ...list[0]);
+
+//         return absolutePath;
+//       }
+//     }
+//   },
+
+//   js: ["node_modules"],
+//   /* ... */
+// };
 
 module.exports = {
-  entry: {
-    main: "./src/es6/main.js",
-  },
+  mode: MODE,
+
+  entry: ENTRY_FILE,
+
   output: {
-    path: path.resolve(__dirname, "./public/src/es5"),
-    filename: "main.js",
+    path: OUTPUT_DIR,
+    filename: "[name].js",
   },
+
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.js$/,
+  //       exclude: path.resolve(__dirname, "node_modules"),
+  //       // exclude: exclude.exportList("js"),
+  //       use: {
+  //         loader: "babel-loader",
+  //         options: {
+  //           presets: ["env"],
+  //         },
+  //       },
+  //     },
+  //   ],
+  // },
 };
