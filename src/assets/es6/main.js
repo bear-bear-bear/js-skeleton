9325afc17e2abcd1f8ce715a64dfee453b2cfa26
bearbearbear <ghlwkdthfl@gmail.com>
2020-09-15 03:36:28 +0900

    feat: 가독성 향상을 위해 호출 요소들을 확장자별로 분리

--- src/assets/es6/main.js
+++ src/assets/es6/main.js
@@ -1,7 +1,10 @@
+/*-- css --*/
 import css from "../scss/main.scss";
+
+/*-- js --*/
 import jsTest from "./Test";
 
 js.log("Hello webpack");
 
-/* ---------- */
+/*-- pug --*/
 import htmlTest from "../../views/test.pug";
