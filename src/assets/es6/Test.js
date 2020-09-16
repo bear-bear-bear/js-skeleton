dec2f5291be62f88ff23272aa3b2ea7e47dbce42
bearbearbear <ghlwkdthfl@gmail.com>
2020-09-17 00:27:41 +0900

    feat: js와 pug 작업구조 프로토타입 설계
    -view/screen의 파일들은 각  페이지를 담당하며, 웹펙 config 에서 각 페이지 파일 이름대로 엔트리 포인트 생성
    -해당 엔트리 포인트는 각 페이지 파일 이름대로 생성한 es6/pages/의 js파일들을 가리키며
    -해당 js 파일들은 각 페이지에 필요한 요소만 import
    -로딩속도를 빠르게한다
    -웹팩이 각 페이지에 해당하는 html파일과 그에 대응하는 각 js파일을 build 아래 확장자별로 구분하여 생성
    -fix;
    -pug를 html로 컴파일하는 과정과 해당 html에 스크립트 코드를 추가하는 과정이 코드상 동시작업으로 취급되어
    -스크립트 코드를 주입하는 코드가 html파일을 찾을 수 없는 오류 해결 중
    -웹팩을 한번 빌드  후 (오류가 뜬다) 다시 한번 빌드하면  첫 실행때 생성된 html 파일에 코드가 정상 주입됨
    -스크립트를 주입하는 과정을 pug>html 컴파일이 끝난 후 실행시키기, 그리고
    -이미 같은 스크립트가 있다면 주입하지 않게하는 함수 (webpack 여러번 실행 시 같은 스크립트가
    -계속해서 생성되는 문제) 생성 작업 필요

--- src/assets/es6/Test.js
+++
@@ -1,5 +0,0 @@
-export default class Test {
-  static log(msg) {
-    console.log("[LOG] " + msg);
-  }
-}
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
-const ENTRY_FILE = path.resolve(__dirname, "src", "assets", "es6", "main.js");
-const OUTPUT_DIR = path.resolve(__dirname, "build");
 const MODE = "development";
+const OUTPUT_DIR = path.resolve(__dirname, "build");
+
+// pug에서 html로 컴파일을 해주면 그 html에 스크립트 코드를 추가하는 함수
+// 아래 html-webpack-plugin 플러그인에서 이 함수 사용
+let htmlPageNames = ["index", "foobar", "fooooo"]; // index는 따로 처리
+let multipleHtmlPlugins = htmlPageNames.map((name) => {
+  return new HtmlWebpackPlugin({
+    template: `${OUTPUT_DIR}/html/${name}.html`, // relative path to the HTML files
+    filename: `html/${name}.html`, // output HTML files
+    chunks: [`${name}`], // respective JS files
+    // html 파일별 요구하는 스크립트에 따라 청크를 분리하여 아웃풋에서 출력된 청크 이름을 chunks에 기입
+  });
+});
 
 module.exports = {
   mode: MODE,
@@ -17,14 +28,39 @@ module.exports = {
   },
 
   devtool: "inline-source-map",
+  // The side effect of this option is to increase build time
   // mode development ? ‘inline-source-map" : 'hidden-source-map’
 
-  entry: ENTRY_FILE,
+  entry: {
+    // 용도에 따라 js파일을 구분하고,
+    // js 파일을 html 페이지에 별로 청크를 분리하여 작성
+    index: path.resolve(__dirname, "src", "assets", "es6", "pages", "index.js"),
+    foobar: path.resolve(
+      __dirname,
+      "src",
+      "assets",
+      "es6",
+      "pages",
+      "foobar.js"
+    ),
+    fooooo: path.resolve(
+      __dirname,
+      "src",
+      "assets",
+      "es6",
+      "pages",
+      "fooooo.js"
+    ),
+    // example1: [path.resolve(__dirname, "src", "assets", "es6", "ex1.js")
+    // example2: path.resolve(__dirname, "src", "assets", "es6", "ex2.js"),
+    // 각 호출 파일 내부에선 기능별 js를 import
+  },
 
   output: {
+    //  entry 에서 분리한 청크별로 다른 번들파일 출력
     path: OUTPUT_DIR,
-    filename: "es5/bundle.js",
-    publicPath: "./",
+    filename: "es5/[name].js", // 작업예약 200916: 청크해쉬 추가하고 html-webpack-plugin에서 지정하기!!
+    publicPath: "../",
   },
 
   module: {
@@ -82,8 +118,7 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "css/style.css",
     }),
-    new HtmlWebpackPlugin({}),
-  ],
+  ].concat(multipleHtmlPlugins), // pug에서 컴파일되어 나온 html 파일별로 스크립트 코드 주입하여 출력
 };
 
 // module.exports = (env, argv) => {
