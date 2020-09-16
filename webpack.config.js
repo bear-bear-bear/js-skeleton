41d2fd967b92c420171207a2d3a835d193a1f14f
bearbearbear <ghlwkdthfl@gmail.com>
2020-09-16 16:05:32 +0900

    feat: 폰트도 번들링하도록 파일로더 설정

--- webpack.config.js
+++ webpack.config.js
@@ -66,12 +66,14 @@ module.exports = {
         ],
       },
       {
-        test: /\.pug$/,
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: [
-          "file-loader?name=html/[name].html",
-          "extract-loader",
-          "html-loader",
-          "pug-html-loader",
+          {
+            loader: "file-loader",
+            options: {
+              name: "font/[name].[ext]?[hash]", //  (mode == "production") ? name: "../img/[hash].[ext]",
+            },
+          },
         ],
       },
     ],
